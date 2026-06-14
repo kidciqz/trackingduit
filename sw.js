3ebc2939d0e985a4561cd0fae61be5c1f46f14d5
kidciqz <aimansyakirin5@gmail.com>
2026-06-14 15:13:57 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='duitku-v1.1.2';
+const CORE=['/','/index.html'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(CORE).catch(()=>{})))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
